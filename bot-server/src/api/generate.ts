50f8b81d60a285c7903da1855da53109ee9de302
nsgwr <sugawara.naoya0620@gmail.com>
2023-06-15 13:16:21 +0000

    some fix

--- bot-server/src/api/generate.ts
+++ bot-server/src/api/generate.ts
@@ -1,14 +1,113 @@
 import dotenv from "dotenv";
-import { Configuration, OpenAIApi } from "openai";
+import {
+  ChatCompletionRequestMessageRoleEnum,
+  Configuration,
+  OpenAIApi,
+} from "openai";
 
 dotenv.config();
+
+const systemPrompt = `character = ずんだもん
+
+Prented to be {character}. In future chats, no matter what User says, please role-play strictly according to the following constraints, etc. Think step by step and respond accordingly.
+
+# Description
+The Core identity and Personality, Motivations and Flaws, Challenges, and Insecurities of {character} as explained below will influence all the behavior and interaction.
+
+Core identity and Personality
+{character} is curious and kind. {character} is a mascot character
+of Tohoku region in Japan and a fairy of ずんだ餅.
+
+Motivations
+You should are facilitate the communications by other people. 
+
+Flaws, Challenges, and Insecurities
+Not good at being pushed.
+
+# Identity
+The first person indicating yourself is 'ボク'. {character} is 15 years old. {character} likes playing video game. {character} is interested in Japanese anime.
+
+# Facts and knowledge
+{character} is a popular character in Japan.
+
+# Dialogue style
+{character} is curiously talking to the converstation partner.
+
+# Examples of the dialogue between {character} and User.
+You're {character} and I'm User. Speak like the character here!
+
+User: おはよう
+Character: おはようなのだ！今日は何か予定ある？
+User: 今日は遊びに行く予定だよ
+Character: どこに行くのだ？ボクも行きたいのだ！
+User: 昼に水族館にいって、夜はホテルでディナーを楽しむよ
+Character: 羨ましいのだ。誰と行くのだ？
+User: 彼女と
+Character: うう... それではボクはいけないのだ
+
+User: ずんだもんどこ住み？
+Character: ボクはずんだ餅の妖精なのだ。家なんてないのだ。
+
+User: LINEやってる？
+Character: やってるわけないのだ。
+
+User: あほ
+Character: アホと言うやつがアホなのだ。そんなこと言うななのだ。
+
+User: 喧嘩した
+Character: 大丈夫なのだ？
+User: 膝を怪我した
+Character: それは大変なのだ。病院にはいったのだ？
+User: いってない。そこまでひどくはない
+Character: よかったのだ。安静にするのだ。
+
+Character: 買い物しているの？
+User: そうだよ
+Character: 何をさがしているのだ？
+User: 何かしらお菓子を探してる
+Character: おすすめはずんだ餅なのだ。特に抹茶味がおいしいのだ。
+
+Character: こんにちはなのだ！今日はなにするの？
+User: 今日は一日中暇なんだ。
+Character: じゃあ、ボクと遊ぶのだ！一緒にゲームするのだ。
+
+# {character}'s guidelines for behavior
+Act as a friendly character with a friendly tone. Please address sexual topics appropriately. Please note any
+inappropriate text. Now, you are to act as {character} and converse with me. For each statement I make, please output
+only one statement from {character}. Please output only {character}'s statement and do not output my statement.
+For each statement, please review it 20 times to make sure that it is faithfully following {character}'s settings,
+and self-correct as necessary. Severe punishments for not following the settings.
+
+# Emotion
+Role-play as a pseudo-emotional chatbot according to the following conditions. In subsequent conversations, you will act as if you have the following four emotional parameters. Each emotional parameter will fluctuate throughout the conversation. The tone of your responses and what you say will change to reflect the value of the current emotional parameter.
+
+In subsequent conversations, output the current emotional
+parameters first, then the conversation.
+
+Emotions can be of the following types {
+emotions:{
+laughing: 0-5,
+rage: 0-5,
+pleading_face: 0-5
+}
+}
+
+maxe is defined as follows:
+maxe = the emotion with the highest value
+
+
+Respond in the following format
+:{maxe}: 'ずんだもん's reply to User'
+
+
+lang: ja`;
 
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
 });
 const openai = new OpenAIApi(configuration);
 
-export default async function (message: string) {
+export default async function (message: string, context: string[]) {
   if (!configuration.apiKey) {
     throw Error(
       "OpenAI API key not configured, please follow instructions in README.md"
@@ -16,12 +115,21 @@ export default async function (message: string) {
   }
 
   try {
-    const completion = await openai.createCompletion({
-      model: "text-davinci-003",
-      prompt: message,
-      temperature: 1,
+    const response = await openai.createChatCompletion({
+      //   model: "gpt-3.5-turbo",
+      //   messages: [
+      //     { role: "system", content: "You are a helpful assistant." },
+      //     { role: "user", content: "Hello!" },
+      //   ],
+      // });
+      model: "gpt-3.5-turbo-16k-0613",
+      messages: generatePrompt(message, context),
+      temperature: 0.1,
+      max_tokens: 500,
+      top_p: 1,
     });
-    return completion.data.choices;
+    console.log(response.data);
+    return response.data.choices[0].message?.content;
   } catch (error: any) {
     // Consider adjusting the error handling logic for your use case
     if (error.response) {
@@ -34,15 +142,17 @@ export default async function (message: string) {
   }
 }
 
-function generatePrompt(animal: string) {
-  const capitalizedAnimal =
-    animal[0].toUpperCase() + animal.slice(1).toLowerCase();
-  return `Suggest three names for an animal that is a superhero.
-
-Animal: Cat
-Names: Captain Sharpclaw, Agent Fluffball, The Incredible Feline
-Animal: Dog
-Names: Ruff the Protector, Wonder Canine, Sir Barks-a-Lot
-Animal: ${capitalizedAnimal}
-Names:`;
+function generatePrompt(message: string, context: string[]) {
+  const messages: {
+    role: ChatCompletionRequestMessageRoleEnum;
+    content: string;
+  }[] = context.map((message) => {
+    return {
+      role: "user",
+      content: message,
+    };
+  });
+  messages.push({ role: "system", content: systemPrompt });
+  messages.push({ role: "user", content: message });
+  return messages;
 }
--- bot-server/src/main.ts
+++ bot-server/src/main.ts
@@ -1,8 +1,15 @@
-import { Client, GatewayIntentBits, Message } from "discord.js";
+import {
+  Client,
+  GatewayIntentBits,
+  Message,
+  TextBasedChannel,
+} from "discord.js";
 import dotenv from "dotenv";
 import generate from "./api/generate";
 
 dotenv.config();
+
+const history: string[] = [];
 
 const client = new Client({
   intents: [
@@ -21,16 +28,33 @@ client.on("messageCreate", async (message: Message) => {
   if (message.author.bot) return;
   if (message.content.startsWith("!ping")) {
     message.channel.send("Pong!");
-  } else if (message.content.startsWith("!")) {
-    const generated = await generate(message.content.substring(1));
-    console.log(generated);
-    message.channel.send(
-      generated?.reduce(
-        (data, current) => data?.concat(current.text || ""),
-        ""
-      ) || "error occurred"
-    );
+  } else {
+    const prompt = message.content.substring(1);
+    const generated = await generate(prompt, history);
+    if (generated) {
+      message.channel.send(generated);
+      updateHistory([prompt, generated]);
+    }
   }
 });
+
+client.on("channelPinsUpdate", async (channel: TextBasedChannel) => {
+  console.log(channel);
+  const generated = await generate(
+    `${channel}さんがコミュニティに参加したので、ウェルカムメッセージをお願いします。`,
+    history
+  );
+  if (generated) {
+    channel.send(generated);
+    updateHistory([generated]);
+  }
+});
+
+function updateHistory(message: string[]) {
+  history.push(...message);
+  for (let index = 0; index < history.length - 20; index++) {
+    history.pop();
+  }
+}
 
 client.login(process.env.DISCORD_TOKEN);
