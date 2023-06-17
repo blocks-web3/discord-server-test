d5652e69a15fef55e2af1e0102fc4f79cede3454
氏弘一也 <kazuyaujihiro@gmail.com>
2023-06-17 21:34:20 +0900

    modify

--- contribution-analyzer/app/issue_classifier.py
+++ contribution-analyzer/app/issue_classifier.py
@@ -1,4 +1,4 @@
-from typing import Dict, Optional
+from typing import Optional, List
 from langchain.chat_models import ChatOpenAI
 from app.issue_classifier_prompt import ISSUE_CLASSIFIER_PROMPT
 from pydantic import BaseModel, Field
@@ -8,7 +8,7 @@ import re
 class IssueClassifier(BaseModel):
     llm: ChatOpenAI = Field(default=ChatOpenAI(model_name="gpt-3.5-turbo-0613"))
 
-    def evaluate(self, messages) -> Dict[str, str]:
+    def evaluate(self, messages) -> List[str]:
         input_data = self._format_messages(messages)
         result = self.llm.predict(ISSUE_CLASSIFIER_PROMPT.format(messages=input_data))
         return self._format_result(result)
@@ -21,20 +21,15 @@ class IssueClassifier(BaseModel):
             text += f"id: {id}, message: {message}\n"
         return text
 
-    def _format_result(self, result_text: Optional[str]) -> Dict[str, str]:
-        results = {}
+    def _format_result(self, result_text: Optional[str]) -> List[str]:
+        results = []
         if result_text is None or len(result_text) == 0:
             return results
         for row in result_text.split("\n"):
             match = re.search(r"id: (\d+)", row)
             if not match:
                 continue
-            id_number = match.group(1)
-            match = re.search(r"message: (.+)", row)
-            if not match:
-                continue
-            extracted_message = match.group(1)
-            results[id_number] = extracted_message
+            results.append(match.group(1))
         return results
 
 
